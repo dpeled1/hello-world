5c15150385d1f7b06671f6d0329d4462e03574dd
dpeled1 <38384945+dpeled1@users.noreply.github.com>
2018-04-14 15:47:39 -0400

    Update hello.js
    added 'to'

--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@ alert("Hello World!");
 
 
 
-var x = parseFloat(prompt("Enter a value < || > 5", "0"));
+var x = parseFloat(prompt("Enter a value < || > to 5", "0"));
 var y = "";
 y  = (x<5) ? " less than 5!" : (x>5) ? " greater than 5!" : ",aheheheh.. 5";
 console.log(y);
