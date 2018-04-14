87c3e442b173cf3c9b63e6eac857e46ee5537077
dpeled1 <38384945+dpeled1@users.noreply.github.com>
2018-04-14 15:39:57 -0400

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,10 @@
+alert("Hello World!");
+
+
+
+
+var x = parseFloat(prompt("Enter a value < || > 5", "0"));
+var y = "";
+y  = (x<5) ? "less than 5!" : (x>5) ? "greater than 5!" : "Heheheh 5";
+console.log(y);
+alert("You entered " + x + ",which is " + y);
