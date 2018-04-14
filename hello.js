c2635deadb4225ae2a11ac54e69f3dbcf3495c12
dpeled1 <38384945+dpeled1@users.noreply.github.com>
2018-04-14 18:15:26 -0400

    fixed up logic in x = 5 or x = string

--- hello.js
+++ hello.js
@@ -5,6 +5,6 @@ alert("Hello World!");
 
 var x = parseFloat(prompt("Enter a value < || > to 5", "0"));
 var y = "";
-y  = (x<5) ? " less than 5!" : (x>5) ? " greater than 5!" : ",aheheheh.. 5";
+y  = (x<5) ? "less than 5!" : (x>5) ? "greater than 5!" : (x === 5) ? "equal to 5!" : "a string!";
 console.log(y);
 alert("You entered " + x + ",which is " + y);
